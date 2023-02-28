75a01c3c57cfa33cf3c37e3bd60fdb5028e98441
Manas <uniyalmanas@gmail.com>
2023-02-28 11:40:38 +0530

    jontest.js

---
+++ server/jontest.js
@@ -0,0 +1,9 @@
+const ob={
+    name : " manas uniyal",
+    id : " 20011666",
+    sec :"k"
+}
+const jsondata = JSON.stringify(ob);
+console.log(jsondata);
+const objectdata = JSON.parse(jsondata);
+console.log(objectdata);
